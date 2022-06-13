16348b8dc6a71995d20fc505712823aab7cb1a01
harshadsutar <harshadsutar007@gmail.com>
2022-06-13 17:14:42 +0530

    first commit

---
+++ students/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect 
+from django.shortcuts import render
+from .models import Student
+
+# Create your views here.
+def index(request):
+    return render(request,'students/index.html',{
+        'students': Student.objects.all()
+    })
+
+def view_student(request,id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
+
